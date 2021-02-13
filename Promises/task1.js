c5e2cf70835271977b87adc996c91e6d0b3dd6d6
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-02-14 01:46:05 +0200

    finished the first task

---
+++ Promises/task1.js
@@ -0,0 +1,10 @@
+// delay with promise
+
+
+function delay(ms) {
+    return new Promise(resolve =>{
+        setTimeout(resolve,ms);
+    }) 
+  }
+  
+  delay(3000).then(() => alert('runs after 3 seconds'));
